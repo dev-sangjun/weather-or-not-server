cd1314ab67179053c528b3db323e31ac845d7a35
ohyeyoye <33212292+ohyeyoye@users.noreply.github.com>
2020-05-29 09:27:58 +0900

    Update weather.js

--- api/weather.js
+++ api/weather.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const API_KEY = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
 const getWeather = (lat, lon) => {
-  const url = `https://api.openweathermap.org/data/2.5/onecall`;
+  const url = "https://api.openweathermap.org/data/2.5/onecall";
   const config = {
     params: {
       lat,
